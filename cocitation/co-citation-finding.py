344e7dfedcbe8f04ac470981cf7bad9653f4218f
hyyc116 <hyyc116@gmail.com>
2017-03-07 10:33:13 -0500

    unicode

--- cocitation/co-citation-finding.py
+++ cocitation/co-citation-finding.py
@@ -20,7 +20,7 @@ def parse_references_with_index(indexpath):
 
         doi = parse_pmc_doi(doc)
 
-        print doi+"\t"+unicode(headers,'utf-8',errors='ignore')
+        print doi+"\t"+headers.encode('utf-8')
 
 if __name__=="__main__":
     parse_references_with_index(sys.argv[1])
