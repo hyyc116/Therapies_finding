4a29b37f7e768b3f6ea21d35e5a371db7a338a9c
hyyc116 <hyyc116@gmail.com>
2017-03-07 10:24:21 -0500

    add co-citation

--- cocitation/co-citation-finding.py
+++ cocitation/co-citation-finding.py
@@ -18,7 +18,7 @@ def parse_references_with_index(indexpath):
 
         doi = parse_pmc_doi(doc)
 
-        print doi+"\t"+unicode(headers,error='ignore')
+        print doi+"\t"+unicode(headers,errors='ignore')
 
 if __name__=="__main__":
     parse_references_with_index(sys.argv[1])
